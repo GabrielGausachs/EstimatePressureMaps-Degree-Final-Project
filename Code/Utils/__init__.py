5b5bda9a967cffcee63e5856794e3a42e80e6861
Gabriel Gausachs <g4a4b4r4i@gmail.com>
2024-02-16 11:17:14 +0100

    Structure

---
+++ Code/Utils/__init__.py
@@ -0,0 +1,2 @@
+# Here I add the imports of the different files of the folder Utils
+# Example: from Utils.dataloader import Dataloader()
---
+++ Code/main.py
@@ -0,0 +1,1 @@
+# Here I add import Utils and it runs the init file that imports all the objects of the Utils folder
