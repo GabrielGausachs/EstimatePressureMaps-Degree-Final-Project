2c2d022764f22968b912bd50d979e7e5a658a03d
gabrielgausachs <g4a4b4r4i@gmail.com>
2024-04-05 10:56:52 +0200

    commit from server

--- Code/Utils/config.py
+++ Code/Utils/config.py
@@ -26,7 +26,7 @@ EVALUATION = False
 
 # Paths -----------------------------------
 
-PATH_DATASET = 'Local'
+PATH_DATASET = 'Server'
 LOCAL_SLP_DATASET_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),'SLP/danaLab')
 SERVER_SLP_DATASET_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))),'mnt/DADES2/SLP/SLP/danaLab')
 MODELS_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Models/SavedModels")
@@ -44,5 +44,5 @@ LEARNING_RATE = 0.0003
 WEIGHT_DECAY = 1e-5
 MOMENTUM = 0.9
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#DEVICE = torch.device("cpu")
+#DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cpu")
