3ce327c1b1dff6f3bf75b582c2a07f59f9c26d16
Gabriel Gausachs <g4a4b4r4i@gmail.com>
2024-04-10 19:25:22 +0200

    commit

--- Code/Utils/crop_zone.py
+++ Code/Utils/crop_zone.py
@@ -33,7 +33,7 @@ coefficient = target_value / mean_neighbours
 
 print('Coefficient for pixel (43, 90):', coefficient)
 
-for i, patient in enumerate((os.listdir('C:/Users/Gabriel/OneDrive/Escritorio/4t any uni/tfg/SLP/danaLab')[:5])):
+for i, patient in enumerate((os.listdir('C:/Users/Gabriel/OneDrive/Escritorio/4t any uni/tfg/SLP/danaLab')[:10])):
     if os.path.isdir(os.path.join('C:/Users/Gabriel/OneDrive/Escritorio/4t any uni/tfg/SLP/danaLab', patient)):
         print(f'patient: {i}')
         patient_path = os.path.join(
@@ -48,10 +48,27 @@ for i, patient in enumerate((os.listdir('C:/Users/Gabriel/OneDrive/Escritorio/4t
                     random_index = random.randint(0, num_files - 1)
                     random_file = os.listdir(category_path)[random_index]
                     pm = np.load(os.path.join(category_path, random_file))
+                    values = cal_indiv[:, random_index]
+                    if category == 'cover1':
+                        pressure = pm * values[1]
+
+                    elif category == 'cover2':
+                        pressure = pm * values[2]
+
+                    elif category == 'uncover':
+                        pressure = pm * values[0]
+
+                    else:
+                        print(category, 'is not a category')
+                    
+                    hist, bin_edges = np.histogram(pm, bins = range(50))  # You can adjust the number of bins as needed
+                    plt.bar(bin_edges[:-1], hist, width = 1)
+                    plt.xlim(min(bin_edges), max(bin_edges))
+                    plt.show()
                     #plt.imshow(pm)
                     #plt.title('normal image')
                     #plt.show()
-
+                    """
                     mask = np.zeros_like(pm, dtype=bool)   
                     mask[149,61] = True               # set target(s)
                     # boolean indexing
@@ -73,3 +90,4 @@ for i, patient in enumerate((os.listdir('C:/Users/Gabriel/OneDrive/Escritorio/4t
                     #plt.imshow(cropped)
                     #plt.title('cropped image')
                     #plt.show()
+                    """
