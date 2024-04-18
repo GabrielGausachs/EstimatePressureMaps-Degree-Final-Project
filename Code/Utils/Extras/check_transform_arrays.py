b9e1b254d1e9ad6393fd198d37905643b08f0d70
Gabriel Gausachs <g4a4b4r4i@gmail.com>
2024-04-18 12:17:18 +0200

    transform image

--- Code/Utils/Extras/check_transform_arrays.py
+++ Code/Utils/Extras/check_transform_arrays.py
@@ -4,6 +4,7 @@ import os
 import cv2
 import random
 
+
 for i, patient in enumerate(os.listdir('C:/Users/Gabriel/OneDrive/Escritorio/4t any uni/tfg/SLP/danaLab')):
     if os.path.isdir(os.path.join('C:/Users/Gabriel/OneDrive/Escritorio/4t any uni/tfg/SLP/danaLab',patient)):
         print('Patient:', patient)
@@ -19,30 +20,33 @@ for i, patient in enumerate(os.listdir('C:/Users/Gabriel/OneDrive/Escritorio/4t 
 
                 random_index = random.randint(0, len(filenames_ir) - 1)
                 file_ir = os.path.join(category_ir_path,filenames_ir[random_index])
-                file_pm = os.path.join(category_pm_path,filenames_pm[random_index])
-                print('ir array:',file_ir)
-                print('pm array:',file_pm)
-                array_ir = np.load(file_ir)
-                array_pm = np.load(file_pm)
+                print(file_ir)
+                if file_ir == 'C:/Users/Gabriel/OneDrive/Escritorio/4t any uni/tfg/SLP/danaLab\00001\IRraw\cover1\000029.npy':
+                    file_pm = os.path.join(category_pm_path,filenames_pm[random_index])
+                    print('ir array:',file_ir)
+                    print('pm array:',file_pm)
+                    array_ir = np.load(file_ir)
+                    array_pm = np.load(file_pm)
 
-                #transform array_ir
-                x = 29
-                y = 7
-                ancho = 66
-                altura = 140
+                    #transform array_ir
+                    x = 29
+                    y = 7
+                    ancho = 66
+                    altura = 140
 
-                array_ir = array_ir[y:y+altura, x:x+ancho]
-                array_ir = cv2.resize(array_ir, (84, 192))
+                    new_array = array_ir[y:y+altura, x:x+ancho]
+                    new_array = cv2.resize(new_array, (84, 192))
 
-                fig, axs = plt.subplots(1, 2)
+                    fig, axs = plt.subplots(1, 2)
 
-                # Plot the array from file_ir
-                axs[0].imshow(array_ir)
-                axs[0].set_title('Array from IR')
+                    # Plot the array from file_ir
+                    axs[0].imshow(array_ir)
+                    axs[0].set_title('Inicial LWIR')
 
-                # Plot the array from file_pm
-                axs[1].imshow(array_pm)
-                axs[1].set_title('Array from PM')
+                    # Plot the array from file_pm
+                    axs[1].imshow(new_array)
+                    axs[1].set_title('LWIR corrected')
 
-                # Show the plot
-                plt.show()
+                    # Show the plot
+                    plt.show()
+    break
