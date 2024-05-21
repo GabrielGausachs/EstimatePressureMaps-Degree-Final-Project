74e994cfc08d7668afe850f9b7129ba87baf0287
Gabriel Gausachs <g4a4b4r4i@gmail.com>
2024-05-21 21:36:50 +0200

    analysis num parameters

--- Code/Utils/Extras/trials.py
+++ Code/Utils/Extras/trials.py
@@ -1,5 +1,7 @@
 import torch
+from matplotlib import pyplot as plt
 
+"""
 # Generate a tensor with random integer values
 tar= torch.randint(low=0, high=100, size=(2, 10, 2), dtype=torch.int)
 src = torch.randint(low=0, high=100, size=(2, 10, 2), dtype=torch.int)
@@ -19,3 +21,19 @@ count = torch.sum(diff < max_values_per_image.unsqueeze(1).unsqueeze(2).expand(2
 pcs = count / tar.numel()
 
 print(pcs)
+"""
+params = [31055183,7762465,1942289,486409]
+values = [1.585,1.661,1.81,1.831]
+acc = [0.9048,0.8996,0.8955,0.8876]
+
+# Create the plot
+plt.figure(figsize=(10, 6))
+plt.plot(params, acc, marker='o', linestyle='-', color='b')
+
+# Add labels and title
+plt.xlabel('Number of Parameters')
+plt.ylabel('PerCS')
+plt.title('Model PerCS vs. Number of Parameters')
+
+# Show the plot
+plt.show()
