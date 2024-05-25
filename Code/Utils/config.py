26e3314b0c1af1755198e8c6836a8302cbdfedbe
gabrielgausachs <g4a4b4r4i@gmail.com>
2024-05-25 09:03:15 +0200

    exexphy

--- Code/Utils/config.py
+++ Code/Utils/config.py
@@ -6,13 +6,13 @@ import torch
 # Paths
 # -----------------------------------------
 
-EXECUTION_NAME = "Final-UNetPhy-MinMax-MSELoss-ScaledNormInput01"
+EXECUTION_NAME = "Final-UNetPhy-PLoss-MinMax-MSELoss-ScaledNormInput01"
 MODEL_NAME = "UNet_phy" #UNet #UNet_phy
 MAX_FEATURE = 1024
 OPTIMIZER = "Adam"
 CRITERION = "MSELoss" #UVLoss #HVLoss #MSELoss 
-PLOSS = False
-WEIGHTSLOSSES = [1,4]
+PLOSS = True
+WEIGHTSLOSSES = [1,1]
 METRIC = "PerCS" #PerCS #MSELoss #MSEeff
 EXPERTYPE = 'Pdata-MinMax-ScaledNormInput01-globalmaxmin'
 WANDB = True
